d2d995195c41639b4c5a0ac9c055d85d303e91cc
Austin Judkins <snikdujad@yahoo.com>
2023-04-26 11:59:24 -0600

    updates

--- login.cmd
+++ login.cmd
@@ -1,1 +1,5 @@
 echo "Logging in"
+
+curl -v -d "@login.json" POST -H "Content-Type:application/json" https://dev.stedi.me/login
+
+curl https://dev.stedi.me/validate/6febf8c9-19e7-4418-8429-658c3f7ff84d
