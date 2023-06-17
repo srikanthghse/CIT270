35e5ca0cc79a7983939d112ae9c980b7813ce91b
Austin Judkins <snikdujad@yahoo.com>
2023-06-17 07:39:38 -0600

    updates

--- login.cmd
+++ login.cmd
@@ -1,5 +1,3 @@
 echo "Logging in"
 
-curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
-
-REM curl https://dev.stedi.me/validate/6febf8c9-19e7-4418-8429-658c3f7ff84d
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" https://austinjudkins.cit270.com/login
